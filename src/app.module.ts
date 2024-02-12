94dba17b4bd2c98a132369fdbf7d0e00f6d1b149
PromotezCitizen <gkstkdgus821@gmail.com>
2024-02-12 20:29:17 +0900

    초기 필요한 모듈들 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentModule } from './comment/comment.module';
+import { ReportModule } from './report/report.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [],
+  imports: [CommentModule, ReportModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
